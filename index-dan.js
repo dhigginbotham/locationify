56e3bedbb28334ba69d41019cbb2e4f509a2f0a5
David Higginbotham <davehigginbotham@gmail.com>
2016-01-08 16:55:52 -0700

    this is gg

--- index-dan.js
+++ index-dan.js
@@ -1,54 +1,26 @@
-var urlPattern = /(\w+):\/*([^\/]+)([a-z0-9\-@\^=%&;\/~\+]*)[\?]?([^ \#]*)#?([^ \#]*)/i;
+var urlPattern = //*([^\/]+)([a-z0-9\-@\^=%&;\/~\+]*)[\?]?([^ \#]*)#?([^ \#]*)/i;
 // var urlPattern = new RegExp('(\w+):/*([^\/]+)([a-z0-9\-@\^=%&;/~\+]*)[\?]?([^ \#]*)#?([^ \#]*)', 'i');
 var partialPattern = /\/*([^\/]*)([a-z0-9\-@\^=%&;\/~\+]*)[\?]?([^ \#]*)#?([^ \t\n\#]*)/i;
 // urlPattern = new RegExp('(\w+):/*([^\/]+)([a-z0-9\-@\^=%&;/~\+]*)[\?]?([^ \#]*)#?([^ \#]*)', 'i');
 
-module.exports = function locationify(url) {
-  if ( url && typeof url === 'string') {
-    var parts = url.match(urlPattern); // well formed, best case (no checks for weird edge case stuff yet)
-    // console.warn('parts', url, parts);
-    if (parts && parts[1]  && parts[2]  && parts[3] ) {
-      if (parts[1] === 'file') {
-        return {
-          protocol: parts[1]+':',
-          host: null,
-          path: (parts[2] + '' + parts[3]).split('/').filter(function(s) { return s; }),
-          query: null,
-          hash: null,
-        };
-      } else {
-        return {
-          protocol: parts[1]+':',
-          host: parts[2],
-          path: parts[3] && parts[3].split('/').filter(function(s) { return s; }),
-          query: parts[4],
-          hash: parts[5],
-        };
-      }
-
-    }
-    if (!parts || !parts.protocol) {
-      parts = url.match(partialPattern);
-      if (parts) {
-        return {
-          protocol: null,
-          host: '',
-          path: (parts[1] + '' + parts[2]).split('/').filter(function(s) { return s; }),
-          // parts[1] && parts[1].split('/').filter(function(s) { return s; }),
-          query: parts[3],
-          hash: parts[4],
-        };
-      }
-    }
-    if (!parts) {
-      parts = url.split(/(\w+):\/*([^ ]*)/);
-      if (parts) {
-        return { protocol: parts[1]+':', path: parts[2] && parts[2].split('/') };
-      }
-
-    }
+var groupList = [{
+  key: 'host',
+  test: function(uri) {
+    var out = uri.match(/(\w+):\//i);
+    return out; 
   }
+}];
 
-  return locationify;
+module.exports = function locationify(url) {
+  var ify = groupList.reduce(function(loc,step){
+    if (typeof step.test == 'function') {
+      loc[step.key] = step.test(url);
+    } else if (step.test instanceof RegExp) {
+      loc[step.key] = url.match(step.test);
+      if (Array.isArray(loc[step.key])) loc[step.key] = loc[step.key][0];
+    }
+    return loc;
+  },{});
+  return ify;
 };
 
